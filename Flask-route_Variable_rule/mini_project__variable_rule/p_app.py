e26e3c428d0a8679244fcb9dea6b0e5cfb6c2424
19coderpawan <coderpawankushwaha@gmail.com>
2025-07-11 02:08:07 +0530

    completed project

--- Flask-route_Variable_rule/mini_project__variable_rule/p_app.py
+++ Flask-route_Variable_rule/mini_project__variable_rule/p_app.py
@@ -2,6 +2,15 @@ from flask import Flask,render_template,request,redirect,url_for
 
 app=Flask(__name__)
 
+@app.route("/pass/<score>")
+def Pass(score):
+    return f"You passed the exam and your score is {score}"
+    
+@app.route("/fail/<score>")
+def Fail(score):
+    return f"You failed the exam and your score is {score}"
+
+
 @app.route("/form",methods=["GET","POST"])
 def form():
     if request.method=="GET":
@@ -12,7 +21,13 @@ def form():
        science=float(request.form['science'])
 
        score=(maths+english+science)/3 
-       return render_template('form.html',score=score)   
+       res=""
+       if score>=45:
+           res="Pass"
+       else:
+           res="Fail" 
+
+       return redirect(url_for(res,score=score))         
 
 if __name__=="__main__":
     app.run(debug=True)
