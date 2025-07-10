d6726cd74810168b63a67b1a8151b2828f4d53e6
19coderpawan <coderpawankushwaha@gmail.com>
2025-07-11 01:55:55 +0530

    updated

--- Flask-route_Variable_rule/mini_project__variable_rule/p_app.py
+++ Flask-route_Variable_rule/mini_project__variable_rule/p_app.py
@@ -1,2 +1,19 @@
 from flask import Flask,render_template,request,redirect,url_for
 
+app=Flask(__name__)
+
+@app.route("/form",methods=["GET","POST"])
+def form():
+    if request.method=="GET":
+        return render_template('form.html')
+    else:
+       maths=float(request.form['maths'])
+       english=float(request.form['english'])
+       science=float(request.form['science'])
+
+       score=(maths+english+science)/3 
+       return render_template('form.html',score=score)   
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
