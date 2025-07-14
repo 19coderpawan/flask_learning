8c2f0237371b57411b0f5dee3e2ef65072e82486
19coderpawan <coderpawankushwaha@gmail.com>
2025-07-14 23:35:53 +0530

    updated with more queries

--- SQLAlchemy_SQLite/SQLAlchemy_core_demo/sqlAlchemy_demo.py
+++ SQLAlchemy_SQLite/SQLAlchemy_core_demo/sqlAlchemy_demo.py
@@ -17,10 +17,23 @@ metadata.create_all(engine)
 # insert_statement=db.insert(Student).values(id=1,name="pawan",branch="mca")
 # result=conn.execute(insert_statement)
 
+
+# conn.commit()
+
+#to update the row.
+update_statement=db.update(Student).where(Student.c.id==1).values(name="pawan kushwaha")
+conn.execute(update_statement)
+conn.commit()
+
 # to select the data.
 # result=conn.execute(Student.select()).fetchall()
-result=conn.execute(Student.select().where(Student.c.name=="pawan")).fetchall() #with where clause.
+result=conn.execute(Student.select().where(Student.c.id=="1")).fetchall() #with where clause.
 for row in result:
     print(f"id={row[0]},name={row[1]},branch={row[2]}")
     # it will print out the data in console window.
-# conn.commit()
+
+# to delete the row form the table.
+delete_statement=db.delete(Student).where(Student.c.id==1)
+conn.execute(delete_statement)
+conn.commit() 
+
