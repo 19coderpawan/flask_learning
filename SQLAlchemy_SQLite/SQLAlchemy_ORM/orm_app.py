cae52fab8ca4688a1840a6aa3725516aad858c1c
19coderpawan <coderpawankushwaha@gmail.com>
2025-07-16 02:14:01 +0530

    updated

--- SQLAlchemy_SQLite/SQLAlchemy_ORM/orm_app.py
+++ SQLAlchemy_SQLite/SQLAlchemy_ORM/orm_app.py
@@ -9,3 +9,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db=SQLAlchemy(app)
+
+# Model(table) 
+class Student_table(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    name=db.Column(db.String(20),nullable=False)
+    branch=db.Column(db.String(20),nullable=False)
+
+    def __repr__(self):
+        return f"<Student{self.name}>"
+
+if __name__=="__main__":
+    app.run(debug=True)
