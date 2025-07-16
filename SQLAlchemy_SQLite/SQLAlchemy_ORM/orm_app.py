93f9c44717ae3d8aa9febaee4c8bf8bf45d73534
19coderpawan <coderpawankushwaha@gmail.com>
2025-07-17 01:06:47 +0530

    added more queries

--- SQLAlchemy_SQLite/SQLAlchemy_ORM/orm_app.py
+++ SQLAlchemy_SQLite/SQLAlchemy_ORM/orm_app.py
@@ -16,6 +16,9 @@ class Student_table(db.Model):
     name=db.Column(db.String(20),nullable=False)
     branch=db.Column(db.String(20),nullable=False)
 
+    # It’s a special Python method:
+    # ells Python how to display the object in interactive shell / print() / logs.
+    # It’s purely for human-friendly debugging.
     def __repr__(self):
         return f"<Student{self.name}>"
 
